9cf91d7bffbd1642d847d9f52f4d48d1fc1a53ce
Farid Ikhadriouene <ikhadriouene@yahoo.fr>
2025-11-06 09:21:52 +0100

    Création de la classe Movie

---
+++ TP_Docker/read/models/movie.py
@@ -0,0 +1,20 @@
+# * L'attribut de classe :
+#     * `id` (int) (il sera incrémenté dans le constructeur mais attention il commencera à 31 -> movie.csv)
+
+# * Les attributs :
+
+#   * `titre` (str)
+#   * `annee_production` (int)
+#   * `genre` (str)
+#   * `age_limite` (int)
+# * Une méthode spéciale `__str__` qui affichera les informations du film de manière lisible.
+
+
+class Movie :
+    def __init__(self, id :int, titre : str, annee_production : int, genre : str, age_limite : int):
+        self.id = 31
+        self.titre = titre
+        self.annee_production = annee_production
+        self.genre = genre
+        self.age_limite = age_limite
+        
